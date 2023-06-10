109194b1313e24a56e6da36c2ecbfa13b4a02ce5
Jonathan Siu <jsiu1248@gmail.com>
2023-06-10 15:20:18 -0700

    editting the function's return values

--- practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/search.js
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/search.js
@@ -2,28 +2,33 @@ export function findElementById(id) {
     // Return the element in the DOM with corresponding `id`
 
     // Your code here
+    var elementId = document.getElementById('id');
 }
 
 export function findFirstElementOfTag(tag) {
     // Return the first occurence of an element of tag name `tag`
 
     // Your code here
+    var firstTag = document.getElementsByTagName('tag')[0];
 }
 
 export function findFirstElementOfClass(cls) {
     // Return the first occurence of an element of class `cls`
 
     // Your code here
+    var firstCls = document.getElementsByTagName('cls')[0];
 }
 
 export function findElementsOfTag(tag) {
     // Return an array of elements that have a tag name of `tag`
 
     // Your code here
+    var elementsTag = document.getElementsByTagName('tag');
 }
 
 export function findElementsOfClass(cls) {
     // Return an array of elements that have are of class `cls`
 
     // Your code here
+    var elementsClassCls = document.getElementsByClassName('cls');
 }
