6c95350d0ae81e13ead601b1cdf93ef42555cdbc
Jonathan Siu <jsiu1248@gmail.com>
2023-06-10 14:31:27 -0700

    adding js projects

---
+++ practice-for-week-09-window-object-and-debugger-long-practice-main/assets/js/manipulate.js
@@ -0,0 +1,18 @@
+export function changeTitle() {
+    // Change the title of the page to "(Your name)'s Portfolio"
+
+    // Your code here
+}
+
+export function changeHeader() {
+    // Change the name in the h1 of the page to your name
+
+    // Your code here
+}
+
+export function changeAboutMe() {
+    /* Update the first paragraph in the About Me section with a small
+     passage about yourself */
+
+    // Your code here
+}
